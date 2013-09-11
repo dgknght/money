2354cbabfbc76e4ad91bdc8de6df0529c54da9ed
unknown <Doug.Knight@VTX-262.corp.veritix.com>
2013-09-11 09:42:47 -0500

    corrected typo in user step

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,3 @@
 Given(/^there is a user with email address "([^"]+)" and password "([^"]+)"$/) do |email, password|
-  User.find_by_email(email) || User.create!(:email => email, :password => password, :password_confirmatio => password)
+  User.find_by_email(email) || User.create!(:email => email, :password => password, :password_confirmation => password)
 end
