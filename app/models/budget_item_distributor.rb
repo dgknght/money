ef1b696233554fd2e3bace4670ad88e2d933d445
Doug Knight <dougbknight@gmail.com>
2013-10-25 15:20:39 -0500

    added direct and total budget item distribution methods

--- app/models/budget_item_distributor.rb
+++ app/models/budget_item_distributor.rb
@@ -1,10 +1,30 @@
 class BudgetItemDistributor
   def self.distribute(budget_item, method, *args)
-    distribute_average(budget_item, *args) if method == :average
+    full_method_name = "distribute_#{method}"
+    raise "unrecognized method '#{method}'" unless respond_to?(full_method_name)
+    send full_method_name, budget_item, *args
   end
   
   private
     def self.distribute_average(budget_item, amount)
       budget_item.periods.each { |p| p.budget_amount = amount }      
+    end
+    
+    def self.distribute_direct(budget_item, amounts)
+      validate_amounts budget_item, amounts
+      
+      amounts.each_with_index { |a, i| budget_item.periods[i].budget_amount = a }
+    end
+    
+    def self.distribute_total(budget_item, total)
+      amount = total / budget_item.periods.length
+      budget_item.periods.each { |p| p.budget_amount = amount }
+    end
+    
+    def self.validate_amounts(budget_item, amounts)
+      raise "amounts must be an array" unless amounts.respond_to?('length')
+      unless budget_item.periods.length == amounts.length
+        raise "incorrect number of elements. expected #{budget_item.periods.length}, received #{amounts.length}"
+      end
     end
 end
--- spec/models/budget_item_distributor_spec.rb
+++ spec/models/budget_item_distributor_spec.rb
@@ -13,4 +13,23 @@ describe BudgetItemDistributor do
       end
     end
   end
+  
+  describe 'distribute (total)' do
+    it 'should distribute the amount specified across all periods in the budget item' do
+      BudgetItemDistributor.distribute(budget_item, :total, 1200)
+      budget_item.periods.each do |period|
+        period.budget_amount.should == 100
+      end
+    end
+  end
+  
+  describe 'distribute (direct)' do
+    it 'should distribute the amount specified across all periods in the budget item' do
+      amounts = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111]
+      BudgetItemDistributor.distribute(budget_item, :direct, amounts)
+      budget_item.periods.each_with_index do |period, index|
+        period.budget_amount.should == amounts[index]
+      end
+    end
+  end
 end
