6d435ddf1094967c11c1bed5ce0bad1c42b7b59c
Doug Knight <dougbknight@gmail.com>
2013-12-16 15:53:50 -0600

    [#61825190] corrected regex definition in format method

--- app/assets/javascripts/strings.js
+++ app/assets/javascripts/strings.js
@@ -2,7 +2,7 @@
  * String extension methods
  */
 String.prototype.format = function(args) {
-  return this.replace(/{([^{}]*)}/, function (fullMatch, subMatch) {
+  return this.replace(/{([^{}]*)}/g, function (fullMatch, subMatch) {
     var value = args[subMatch];
     return (typeof value === 'string' || typeof value === 'number') ? value : fullMatch;
   });
