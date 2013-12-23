d2c7d51c15c64b5ed4658309d3ffedf0fa94c932
Doug Knight <dougbknight@gmail.com>
2013-12-23 10:54:13 -0600

    [#62864338] added transaction details

--- app/assets/javascripts/transaction_item_view_model.js
+++ app/assets/javascripts/transaction_item_view_model.js
@@ -6,9 +6,30 @@ function TransactionItemViewModel(transaction_item, transaction) {
   this.amount = ko.observable(transaction_item.amount);
   this.reconciled = ko.observable(transaction_item.reconciled);
   this.previousItem = ko.observable();
+  this.showDetails = ko.observable(false);
+
+  this.toggleDetails = function() {
+    if (this.showDetails()) {
+      this.details.removeAll();
+      this.showDetails(false);
+    } else {
+      this.transaction.items().pushAllTo(this.details);
+      this.showDetails(true);
+    }
+  };
+
+  this.toggleCss = ko.computed(function() {
+   return this.showDetails() ? "ui-icon-triangle-1-s" : "ui-icon-triangle-1-e";
+  }, this);
+
+  this.details = ko.observableArray();
 
   this.account = ko.computed(function() {
     return this.transaction.entity.getAccount(this.account_id());
+  }, this);
+
+  this.accountName = ko.computed(function() {
+    return this.account().name();
   }, this);
 
   this.formattedAmount = ko.computed(function() {
@@ -22,6 +43,62 @@ function TransactionItemViewModel(transaction_item, transaction) {
   this.formattedPolarizedAmount = ko.computed(function() {
     return accounting.formatNumber(this.polarizedAmount(), 2);
   }, this);
+
+  this.creditAmount = ko.computed({
+    read: function() {
+      return this.action() == 'credit' ? this.amount() : 0;
+    },
+    write: function(value) {
+      if (value != 0) {
+        this.amount(value);
+        this.action('credit');
+      }
+    },
+    owner: this
+  });
+
+  this.formattedCreditAmount = ko.computed({
+    read: function() {
+      var value = this.creditAmount();
+      return value == 0 ? "" : accounting.formatNumber(value, 2);
+    },
+    write: function(value) {
+      if (value == null || value.length == 0) {
+        this.creditAmount(0);
+      } else {
+        this.creditAmount(parseFloat(value));
+      }
+    },
+    owner: this
+  });
+
+  this.debitAmount = ko.computed({
+    read: function() {
+      return this.action() == 'debit' ? this.amount() : 0;
+    },
+    write: function(value) {
+      if (value != 0) {
+        this.amount(value);
+        this.action('debit');
+      }
+    },
+    owner: this
+  });
+
+  this.formattedDebitAmount = ko.computed({
+    read: function() {
+      var value = this.debitAmount();
+      return value == 0 ? "" : accounting.formatNumber(value, 2);
+    },
+    write: function(value) {
+      if (value == null || value.length == 0) {
+        this.debitAmount(0);
+      } else {
+        this.debitAmount(parseFloat(value));
+      }
+    },
+    owner: this
+  });
 
   this.balance = ko.computed(function() {
     var base = this.previousItem() == null ? 0 : this.previousItem().balance();
