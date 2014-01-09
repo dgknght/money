544d72936e6456b16c6397adc6d00088be3128b9
Doug Knight <dougbknight@gmail.com>
2014-01-09 15:17:23 -0600

    [#62779116] added mockjax framework, added mocks to unit tests, expanded EntityViewModel.accounts test

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,29 +1,60 @@
 //= require qunit-1.13.0
 //= require single_page
+//= require jquery.mockjax
 
-module('MoneyApp');
+module('MoneyApp', {
+  setup: function() {
+    $.mockjax({
+      url: 'entities.json',
+      responseText: [
+        { id: 1, name: 'First Entity' }
+      ]
+    });
+  },
+  teardown: function() {
+    $.mockjaxClear();
+  }
+});
 test("should be creatable", function() {
   ok(new MoneyApp());
 })
 asyncTest("entities", 4, function() {
-  /*
-   * Currenty relying on data already in the database
-   * Need to find a mocking framework so we can control 
-   * what's in the database
-   */
   var app = new MoneyApp();
   ok(app.entities, "It should have an entities property.");
 
-  equal(app.entities(), 0, "The entities should not load until accessed.");
-
   app.entities.subscribe(function(entities) { 
+    if (entities.length == 0) return;
+    if (entities.length > 1 )
+      throw 'Too many entities returned';
+
     equal(app.entities().length, 1, "After loading, it should have one entity.");
-    equal(app.entities()[0].name(), "Personal", "The entity should be called 'Personal'");
+    equal(app.entities()[0].name(), "First Entity", "The entity should be called 'Personal'");
     start();
   });
+
+  equal(app.entities(), 0, "The entities should not load until accessed.");
 })
 
-module('EntityViewmodel');
+module('EntityViewmodel', {
+  setup: function() {
+    $.mockjax({
+      url: 'entities.json',
+      responseText: [
+        { id: 10, name: 'First Entity' }
+      ]
+    });
+    $.mockjax({
+      url: 'entities/10/accounts.json',
+      responseText: [
+        { id: 1, name: 'Checking', account_type: 'asset' },
+        { id: 2, name: 'Salary', account_type: 'income' },
+      ]
+    });
+  },
+  teardown: function() {
+    $.mockjaxClear();
+  }
+});
 test("should be creatable with valid data and a MoneyApp instance", function() {
   var app = new MoneyApp();
   var entity = {
@@ -32,14 +63,28 @@ test("should be creatable with valid data and a MoneyApp instance", function() {
   };
   ok(new EntityViewModel(entity, app));
 })
-asyncTest("should have a list of accounts", 1, function() {
+asyncTest("should have a list of accounts", 4, function() {
   var app = new MoneyApp();
   app.entities.subscribe(function(entities) {
     if (entities.length == 0) return;
+    if (entities.length > 1)
+      throw 'too many entities'
 
     var entity = app.entities().first();
     ok(entity.accounts, "It should have an accounts property.");
-    start();
+
+    entity.accounts.subscribe(function(accounts) {
+      if (accounts.length < 2) return;
+      if (accounts.length > 2)
+        throw 'too many accounts'
+
+      equal(entity.accounts()[0].name(), 'Checking');
+      equal(entity.accounts()[1].name(), 'Salary');
+
+      start();
+    });
+
+    equal(entity.accounts(), 0, "The property should not load until accessed.");
   });
   app.entities();
 })
