c3525ea8d77ae0f44dee63220642c78bcb5156e8
Doug Knight <administrator.knightsoftwareservices.com>
2014-03-17 16:54:02 -0500

    [#64680394] added attachment from to single page app and started fleshing out the save/insert behavior

--- app/assets/javascripts/view_models/attachment_view_model.js
+++ app/assets/javascripts/view_models/attachment_view_model.js
@@ -1,15 +1,31 @@
 function AttachmentViewModel(attachment, transaction) {
   this.transaction = transaction;
+  this.id = ko.observable();
+  this.name = ko.observable();
+  this.content_type = ko.observable();
+  this.attachment_content_id = ko.observable();
+  this.raw_file = ko.observable();
+
   if (attachment) {
-    this.id = ko.observable(attachment.id);
-    this.name = ko.observable(attachment.name);
-    this.content_type = ko.observable(attachment.content_type);
-    this.attachment_content_id = ko.observable(attachment.attachment_content_id);
+    this.id(attachment.id);
+    this.name(attachment.name);
+    this.content_type(attachment.content_type);
+    this.attachment_content_id(attachment.attachment_content_id);
   }
 
   this.show = function() {
     var url = "attachment_contents/{id}".format({ id: this.attachment_content_id() });
     window.open(url, "_blank");
   }
+
+  this.entityIdentifier = function() {
+    return "attachment";
+  };
+  this.entityListPath = function() {
+    return "transactions/{transaction_id}/attachments".format({transaction_id: this.transaction.id()});
+  };
+  this.toJson = function() {
+    return this;
+  };
 }
 AttachmentViewModel.prototype = new ServiceEntity();
