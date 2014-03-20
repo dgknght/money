6469b8321154eed1b5b692c34f45f335c747bdfb
Doug Knight <administrator@knightsoftwareservices.com>
2014-03-19 20:32:10 -0500

    [#64680394] corrected bad url for attachment view model service call

--- app/assets/javascripts/view_models/attachment_view_model.js
+++ app/assets/javascripts/view_models/attachment_view_model.js
@@ -22,10 +22,15 @@ function AttachmentViewModel(attachment, transaction) {
     return "attachment";
   };
   this.entityListPath = function() {
-    return "transactions/{transaction_id}/attachments".format({transaction_id: this.transaction.id()});
+    return "transactions/{transaction_id}/attachments.json".format({transaction_id: _self.transaction.id()});
   };
   this.toJson = function() {
-    return this;
+    return {
+      id: _self.id(),
+      name: _self.name(),
+      content_type: _self.content_type(),
+      attachment_content_id: _self.attachment_content_id()
+    };
   };
 }
 AttachmentViewModel.prototype = new ServiceEntity();
