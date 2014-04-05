0fb6a37066f2f4c8ee508cf3331b0b1c74f2b4e7
Doug Knight <dougbknight@gmail.com>
2014-04-04 21:45:38 -0500

    [#64680394] added delete attachment behavior to single page

--- app/assets/javascripts/view_models/attachment_view_model.js
+++ app/assets/javascripts/view_models/attachment_view_model.js
@@ -14,6 +14,31 @@ function AttachmentViewModel(attachment, transaction) {
     this.attachment_content_id(attachment.attachment_content_id);
   }
 
+  // TODO This is an exact copy of ServiceEntity.destory. it should be shared
+  this.destroy = function(supressConfirmation) {
+
+    console.log("destroy supressConfirmation=" + supressConfirmation);
+
+    if (!supressConfirmation) {
+      var message = "Are you sure you want to delete the selected attachment?"
+      if (!confirm(message)) return;
+    }
+
+    var self = this;
+    var url = "attachments/{id}.json".format({id: _self.id()});
+    $.ajax({
+      url: url,
+      type: 'DELETE',
+      success: function(data, textStatus, jqXHR) {
+        _self.transaction.attachments.remove(_self);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log("error " + errorThrown);
+      },
+      context: self
+    });
+  };
+
   this.save = function(success) {
     success = _.ensureFunction(success);
     var data = new FormData();
