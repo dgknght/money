7cb00bf030233badb2505b8e230670aec95d06f6
Doug Knight <dougbknight@gmail.com>
2014-04-22 14:32:11 -0500

    changed initializer signature for CommodityTransactionCreator

--- app/models/commodity_transaction_creator.rb
+++ app/models/commodity_transaction_creator.rb
@@ -3,15 +3,29 @@ class CommodityTransactionCreator
 
   ACTIONS = %w(buy sell)
 
-  attr_accessor :transaction_date, :symbol, :action, :shares, :value
+  attr_accessor :account_id, :transaction_date, :symbol, :action, :shares, :value
 
-  validates_presence_of :symbol
+  validates_presence_of :symbol, :account_id
   validates :shares, presence: true, numericality: true
   validates :action, presence: true, inclusion: { in: ACTIONS }
   validates :value, presence: true, numericality: true
 
-  def initialize(account, attributes = {})
+  def account
+    @account ||= find_account
+  end
+
+  def account=(account)
+    @account = account
+    self.account_id = account.nil? ? nil : account.id
+  end
+
+  def create
+    return nil unless valid?
+  end
+
+  def initialize(attributes = {})
     attr = (attributes || {}).with_indifferent_access
+    self.account_id = attr[:account_id]
     self.transaction_date = attr.get_date(:transaction_date) || Date.today
     self.action = attr[:action]
     self.symbol = attr[:symbol]
@@ -22,5 +36,12 @@ class CommodityTransactionCreator
   def price
     return nil if value.nil? || shares.nil?
     value / shares
+  end
+
+  private
+
+  def find_account
+    return nil unless account_id
+    Account.find(account_id)
   end
 end
--- spec/models/commodity_transaction_creator_spec.rb
+++ spec/models/commodity_transaction_creator_spec.rb
@@ -4,6 +4,7 @@ describe CommodityTransactionCreator do
   let (:account) { FactoryGirl.create(:account) }
   let (:attributes) do
     {
+      account_id: account.id,
       transaction_date: '2014-04-15',
       symbol: 'KSS',
       action: 'buy',
@@ -13,7 +14,7 @@ describe CommodityTransactionCreator do
   end
 
   it 'should be creatble with an account and valid attributes' do
-    creator = CommodityTransactionCreator.new(account, attributes)
+    creator = CommodityTransactionCreator.new(attributes)
     expect(creator).to be_valid
     expect(creator.transaction_date).to eq(Date.parse('2014-04-15'))
     expect(creator.symbol).to eq('KSS')
@@ -21,10 +22,18 @@ describe CommodityTransactionCreator do
     expect(creator.value).to eq(1_234)
   end
 
+  describe '#account_id' do
+    it 'should be required' do
+      creator = CommodityTransactionCreator.new(attributes.except(:account_id))
+      expect(creator).not_to be_valid
+      expect(creator).to have(1).error_on(:account_id)
+    end
+  end
+
   describe '#transaction_date' do
     it 'should default to the current date' do
       Timecop.freeze(Time.local(2014, 1, 1, 0, 0, 0)) do
-        creator = CommodityTransactionCreator.new(account, attributes.except(:transaction_date))
+        creator = CommodityTransactionCreator.new(attributes.except(:transaction_date))
         expect(creator).to be_valid
         expect(creator.transaction_date).to eq(Date.parse('2014-01-01'))
       end
@@ -33,7 +42,7 @@ describe CommodityTransactionCreator do
 
   describe '#symbol' do
     it 'should be required' do
-      creator = CommodityTransactionCreator.new(account, attributes.except(:symbol))
+      creator = CommodityTransactionCreator.new(attributes.except(:symbol))
       expect(creator).not_to be_valid
       expect(creator).to have(1).error_on(:symbol)
     end
@@ -41,23 +50,23 @@ describe CommodityTransactionCreator do
 
   describe '#action' do
     it 'should be required' do
-      creator = CommodityTransactionCreator.new(account, attributes.except(:action))
+      creator = CommodityTransactionCreator.new(attributes.except(:action))
       expect(creator).not_to be_valid
       expect(creator).to have(2).errors_on(:action)
     end
 
     it 'should accept "buy"' do
-      creator = CommodityTransactionCreator.new(account, attributes.merge(:action => 'buy'))
+      creator = CommodityTransactionCreator.new(attributes.merge(:action => 'buy'))
       expect(creator).to be_valid
     end
 
     it 'should accept "sell"' do
-      creator = CommodityTransactionCreator.new(account, attributes.merge(:action => 'sell'))
+      creator = CommodityTransactionCreator.new(attributes.merge(:action => 'sell'))
       expect(creator).to be_valid
     end
 
     it 'should not accept anything else' do
-      creator = CommodityTransactionCreator.new(account, attributes.merge(action: 'notvalid'))
+      creator = CommodityTransactionCreator.new(attributes.merge(action: 'notvalid'))
       expect(creator).not_to be_valid
       expect(creator).to have(1).error_on(:action)
     end
@@ -65,13 +74,13 @@ describe CommodityTransactionCreator do
 
   describe '#shares' do
     it 'should be required' do
-      creator = CommodityTransactionCreator.new(account, attributes.except(:shares))
+      creator = CommodityTransactionCreator.new(attributes.except(:shares))
       expect(creator).not_to be_valid
       expect(creator).to have(2).errors_on(:shares)
     end
 
     it 'should be a number' do
-      creator = CommodityTransactionCreator.new(account, attributes.merge(shares: 'notanumber'))
+      creator = CommodityTransactionCreator.new(attributes.merge(shares: 'notanumber'))
       expect(creator).not_to be_valid
       expect(creator).to have(1).error_on(:shares)
     end
@@ -79,20 +88,20 @@ describe CommodityTransactionCreator do
 
   describe '#price' do
     it 'should be calculated based on the value and shares' do
-      creator = CommodityTransactionCreator.new(account, attributes)
+      creator = CommodityTransactionCreator.new(attributes)
       expect(creator.price).to eq(12.34)
     end
   end
 
   describe '#value' do
     it 'should be required' do
-      creator = CommodityTransactionCreator.new(account, attributes.except(:value))
+      creator = CommodityTransactionCreator.new(attributes.except(:value))
       expect(creator).not_to be_valid
       expect(creator).to have(2).errors_on(:value)
     end
 
     it 'should be a number' do
-      creator = CommodityTransactionCreator.new(account, attributes.merge(value: 'notanumber'))
+      creator = CommodityTransactionCreator.new(attributes.merge(value: 'notanumber'))
       expect(creator).not_to be_valid
       expect(creator).to have(1).error_on(:value)
     end
@@ -100,9 +109,17 @@ describe CommodityTransactionCreator do
 
   describe '#create' do
     context 'with a "buy" action' do
-      it 'should create a new transaction'
+      it 'should create a new transaction' do
+        expect do
+          creator = CommodityTransactionCreator.new(attributes)
+          transaction = creator.create
+        end.to change(Transaction, :count).by(1)
+        expect(transaction).not_to be_nil
+      end
+
       it 'should debit the account dedicated to tracking purchases of this commodity'
       it 'should credit the account from which frunds for taken to make the purchase'
+      it 'should increase the number of shares held of this commodity'
     end
 
     context 'with a "sell" action' do
