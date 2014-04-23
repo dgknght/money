b0a3468e6406c11f62c5c9df07d20e4c581e2994
Doug Knight <dougbknight@gmail.com>
2014-04-23 14:49:46 -0500

    [#68924418] working prices controller spec

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,17 +1,24 @@
 class PricesController < ApplicationController
+  before_filter :load_commodity, only: [:index, :create]
+  before_filter :load_price, only: [:show]
   respond_to :html, :json
 
   def index
+    @prices = @commodity.prices
     respond_with @prices
   end
 
   def show
+    respond_with(@price)
   end
 
   def new
   end
 
   def create
+    @price = @commodity.prices.new(price_params)
+    flash[:notice] = 'The price was created successfully.' if @price.save
+    respond_with(@price, location: commodity_prices_path(@commodity))
   end
 
   def edit
@@ -22,4 +29,18 @@ class PricesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def load_commodity
+      @commodity = Commodity.find(params[:commodity_id])
+    end
+
+    def load_price
+      @price = Price.find(params[:id])
+    end
+
+    def price_params
+      params.require(:price).permit(:trade_date, :price)
+    end
 end
--- spec/controllers/prices_controller_spec.rb
+++ spec/controllers/prices_controller_spec.rb
@@ -4,6 +4,12 @@ describe PricesController do
   let (:entity) { FactoryGirl.create(:entity) }
   let (:commodity) { FactoryGirl.create(:commodity, entity: entity) }
   let!(:price) { FactoryGirl.create(:price, commodity: commodity) }
+  let (:attributes) do
+    {
+      trade_date: Date.today.iso8601,
+      price: '12.3456'
+    }
+  end
 
   context 'for an authenticated user' do
     context 'that owns the entity' do
@@ -30,26 +36,65 @@ describe PricesController do
       end
 
       describe 'get :show' do
-        it 'should be successful'
+        it 'should be successful' do
+          get :show, id: price
+          expect(response).to be_success
+        end
 
         context 'in json' do
-          it 'should be successful'
-          it 'should return the specified commodity price'
+          it 'should be successful' do
+            get :show, id: price, format: :json
+            expect(response).to be_success
+          end
+
+          it 'should return the specified commodity price' do
+            get :show, id: price, format: :json
+            expect(response.body).to eq(price.to_json)
+          end
         end
       end
 
       describe 'get :new' do
-        it 'should be successful'
+        it 'should be successful' do
+          get :new, commodity_id: commodity
+          expect(response).to be_success
+        end
       end
 
       describe 'post :create' do
-        it 'should redirect to the commodity price index page'
-        it 'should create a new commodity price'
+        it 'should redirect to the commodity price index page' do
+          post :create, commodity_id: commodity, price: attributes
+          expect(response).to redirect_to commodity_prices_path(commodity)
+        end
+
+        it 'should create a new commodity price' do
+          expect do
+            post :create, commodity_id: commodity, price: attributes
+          end.to change(commodity.prices, :count).by(1)
+        end
 
         context 'in json' do
-          it 'should be successful'
-          it 'should create a new commodity price'
-          it 'should return the new commodity price'
+          it 'should be successful' do
+            post :create, commodity_id: commodity, price: attributes, format: :json
+            expect(response).to be_success
+          end
+
+          it 'should create a new commodity price' do
+            expect do
+              post :create, commodity_id: commodity, price: attributes, format: :json
+            end.to change(commodity.prices, :count).by(1)
+          end
+
+          it 'should return the new commodity price' do
+            post :create, commodity_id: commodity, price: attributes, format: :json
+
+            puts "response.body=#{response.body}"
+
+            returned = JSON.parse(response.body)
+            attributes.each do |key, value|
+              expect(returned[key.to_s]).to eq(value)
+            end
+          end
         end
       end
 
