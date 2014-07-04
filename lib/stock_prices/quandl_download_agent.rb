74b2cae5493f39a9e10a8e62cb784f93f21d59b3
Doug Knight <dougbknight@gmail.com>
2014-07-04 13:58:06 -0500

    [#68924438] started adding spec for quandl download agent

---
+++ lib/stock_prices/quandl_download_agent.rb
@@ -0,0 +1,7 @@
+module StockPrices
+  # Downloads stock prices using quandl
+  class QuandlDownloadAgent
+    def download_prices
+    end
+  end
+end
---
+++ spec/lib/stock_prices/quandl_download_agent_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe StockPrices::QuandlDownloadAgent do
+  describe '#download_prices' do
+    it 'should return prices from the web service'
+  end
+end
