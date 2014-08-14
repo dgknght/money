204280481e7288c1341ec2713bc8aaa27df9a53e
Doug Knight <info@knightsoftwareservices.com>
2014-08-13 20:18:47 -0500

    [#75774016] finished working the lot view model spec

--- app/assets/javascripts/tests/lot_view_model_test.js
+++ app/assets/javascripts/tests/lot_view_model_test.js
@@ -24,7 +24,17 @@
       $.mockjax({
         url: 'accounts/' + KSS_ACCOUNT_ID + '/holdings.json',
         responseText: [
-          { id: HOLDING_ID, lots: [ { id: LOT_ID, account_id: KSS_ACCOUNT_ID, commodity_ID: KSS_ID, price: 10, shares_owned: 100, purchase_date: '2014-02-01' } ] }
+          { id: HOLDING_ID, lots: [
+              {
+                id: LOT_ID,
+                account_id: KSS_ACCOUNT_ID,
+                commodity_id: KSS_ID,
+                price: 10,
+                shares_owned: 100,
+                purchase_date: '2014-02-01'
+              }
+            ]
+          }
         ]
       });
     },
@@ -46,15 +56,42 @@
     });
   });
   asyncTest('commodity_id', function() {
-    ok(false, 'Need to write the test.');
-    start();
+    expect(3);
+
+    getHolding(new MoneyApp(), {entity_id: ENTITY_ID, account_id: KSS_ACCOUNT_ID, holding_id: HOLDING_ID}, function(holding) {
+      var lot = _.first(holding.lots());
+      ok(lot, 'The lot should be available in the list of lots for the holding');
+      ok(lot.commodity_id, 'The lot should have an commodity_id property');
+      if (lot.commodity_id) {
+        equal(lot.commodity_id(), KSS_ID, 'The commodity_id property should have the correct value');
+      }
+      start();
+    });
   });
   asyncTest('price', function() {
-    ok(false, 'Need to write the test.');
-    start();
+    expect(3);
+
+    getHolding(new MoneyApp(), {entity_id: ENTITY_ID, account_id: KSS_ACCOUNT_ID, holding_id: HOLDING_ID}, function(holding) {
+      var lot = _.first(holding.lots());
+      ok(lot, 'The lot should be available in the list of lots for the holding');
+      ok(lot.price, 'The lot should have an price property');
+      if (lot.price) {
+        equal(lot.price(), 10, 'The price property should have the correct value');
+      }
+      start();
+    });
   });
   asyncTest('shares_owned', function() {
-    ok(false, 'Need to write the test.');
-    start();
+    expect(3);
+
+    getHolding(new MoneyApp(), {entity_id: ENTITY_ID, account_id: KSS_ACCOUNT_ID, holding_id: HOLDING_ID}, function(holding) {
+      var lot = _.first(holding.lots());
+      ok(lot, 'The lot should be available in the list of lots for the holding');
+      ok(lot.shares_owned, 'The lot should have an shares_owned property');
+      if (lot.shares_owned) {
+        equal(lot.shares_owned(), 100, 'The shares_owned property should have the correct value');
+      }
+      start();
+    });
   });
 })()
--- app/assets/javascripts/view_models/lot_view_model.js
+++ app/assets/javascripts/view_models/lot_view_model.js
@@ -1,5 +1,8 @@
 function LotViewModel(lot) {
   var _self = this;
-  this.id         = ko.observable(lot.id);
-  this.account_id = ko.observable(lot.account_id);
+  this.id           = ko.observable(lot.id);
+  this.account_id   = ko.observable(lot.account_id);
+  this.commodity_id = ko.observable(lot.commodity_id);
+  this.price        = ko.observable(lot.price);
+  this.shares_owned = ko.observable(lot.shares_owned);
 }
