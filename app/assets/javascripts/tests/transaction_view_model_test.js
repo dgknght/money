e526d684d418817beeb37044f6df3839153aa7bb
Doug Knight <dougbknight@gmail.com>
2014-09-16 18:39:01 -0500

    cleaned up transaction item view model test

--- app/assets/javascripts/tests/transaction_view_model_test.js
+++ app/assets/javascripts/tests/transaction_view_model_test.js
@@ -1,229 +1,250 @@
-module('TransactionViewModel', {
-  setup: function() {
-    $.mockjax({
-      url: 'entities.json',
-      responseText: [
-        { id: 10, name: 'First Entity' }
-      ]
-    });
-    $.mockjax({
-      url: 'entities/10/accounts.json',
-      responseText: [
-        { id: 1, name: 'Checking', account_type: 'asset' },
-        { id: 2, name: 'Salary', account_type: 'income' },
-        { id: 3, name: 'Income Tax', account_type: 'expense' }
-      ]
-    });
-    $.mockjax({
-      url: 'transactions/*/attachments.json',
-      responseText: []
-    });
-    $.mockjax({
-      url: 'entities/10/transactions.json?account_id=1',
-      responseText: [
-        { 
-          id: 1001, 
-          transaction_date: '2014-01-01',
-          description: 'Salary',
-          items: [
-            { id: 10001, account_id: 1, action: 'debit', amount: "1000" },
-            { id: 10002, account_id: 2, action: 'credit', amount: "1000" }
-          ]
-        },
-      ]
-    });
-  },
-  teardown: function() {
-    $.mockjaxClear();
-  }
-});
-asyncTest("transaction_date", function() {
-  var app = new MoneyApp();
-  getAccount(app, { entity_id: 10, account_id: 1 }, function(account) {
-    getFromLazyLoadedCollection(account, "transaction_items", 10001, function(item) {
-      ok(item.transaction_item, "The item rollup should reference the underlying transaction item.")
-      ok(item.transaction_item.transaction, "The transaction item should reference the transaction.")
+(function() {
+  var ENTITY_ID = 10;
+  var CHECKING_ID = 1;
+  var SALARY_ID = 2;
+  var INCOME_TAX_ID = 3;
+  var TRANSACTION_ID = 1001;
+  var TRANSACTION_ITEM_1_ID = 10001;
+  var TRANSACTION_ITEM_2_ID = 10002;
 
-      var transaction = item.transaction_item.transaction;
-      ok(transaction.transaction_date(), "The tranaction should have a transaction_date property.");
-      equal(transaction.transaction_date().toLocaleDateString(), "1/1/2014", "The transaction_date property should have the correct value.");
+  module('TransactionViewModel', {
+    setup: function() {
+      $.mockjaxClear();
+      $.mockjaxSettings.throwUnmocked = true;
+      $.mockjax({
+        url: 'entities.json',
+        responseText: [
+          { id: ENTITY_ID, name: 'First Entity' }
+        ]
+      });
+      $.mockjax({
+        url: 'entities/' + ENTITY_ID + '/accounts.json',
+        responseText: [
+          { id: CHECKING_ID, name: 'Checking', account_type: 'asset' },
+          { id: SALARY_ID, name: 'Salary', account_type: 'income' },
+          { id: INCOME_TAX_ID, name: 'Income Tax', account_type: 'expense' }
+        ]
+      });
+      $.mockjax({
+        url: 'transactions/*/attachments.json',
+        responseText: []
+      });
+      $.mockjax({
+        url: 'entities/' + ENTITY_ID + '/transactions.json?account_id=' + CHECKING_ID,
+        responseText: [
+          {
+            id: TRANSACTION_ID,
+            transaction_date: '2014-01-01',
+            description: 'Salary',
+            items: [
+              { id: TRANSACTION_ITEM_1_ID, account_id: CHECKING_ID, action: 'debit', amount: "1000" },
+              { id: TRANSACTION_ITEM_2_ID, account_id: SALARY_ID, action: 'credit', amount: "1000" }
+            ]
+          },
+        ]
+      });
+      $.mockjax({
+        url: 'accounts/*/lots.json',
+        responseText: []
+      });
+      $.mockjax({
+        url: 'transactions/*.json',
+        type: 'PUT',
+        responseText: []
+      });
+    },
+    teardown: function() {
+      $.mockjaxClear();
+    }
+  });
+  asyncTest("transaction_date", function() {
+    var app = new MoneyApp();
+    getAccount(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID }, function(account) {
+      getFromLazyLoadedCollection(account, "transaction_items", 10001, function(item) {
+        ok(item.transaction_item, "The item rollup should reference the underlying transaction item.")
+        ok(item.transaction_item.transaction, "The transaction item should reference the transaction.")
+
+        var transaction = item.transaction_item.transaction;
+        ok(transaction.transaction_date(), "The tranaction should have a transaction_date property.");
+        equal(transaction.transaction_date().toLocaleDateString(), "1/1/2014", "The transaction_date property should have the correct value.");
+        start();
+      });
+    });
+  });
+  asyncTest("creditAmount", function() {
+    expect(2);
+
+    var app = new MoneyApp();
+    getAccount(app, { entity_id: ENTITY_ID, account_id: INCOME_TAX_ID }, function(account) {
+      var transaction = {
+        transaction_date: "2014-02-27",
+        description: 'Paycheck', 
+        items: [
+          { account_id: CHECKING_ID, action: 'debit', amount: 1000 },
+          { account_id: SALARY_ID, action: 'credit', amount: 1000 }
+        ]
+      };
+      var viewModel = new TransactionViewModel(transaction, account.entity);
+
+      equal(viewModel.creditAmount(), 1000, "creditAmount should be the sum of all the credit items.");
+
+      var item = _.find(viewModel.items(), function(item) { return item.account_id() == SALARY_ID; });
+      item.amount(987);
+
+      equal(viewModel.creditAmount(), 987, "creditAmount should change when an item with a 'credit' action changes");
+
       start();
     });
   });
-});
-asyncTest("creditAmount", function() {
-  expect(2);
+  asyncTest("formattedCreditAmount", function() {
+    expect(2);
 
-  var app = new MoneyApp();
-  getAccount(app, { entity_id: 10, account_id: 3 }, function(account) {
-    var transaction = {
-      transaction_date: "2014-02-27",
-      description: 'Paycheck', 
-      items: [
-        { account_id: 1, action: 'debit', amount: 1000 },
-        { account_id: 2, action: 'credit', amount: 1000 }
-      ]
-    };
-    var viewModel = new TransactionViewModel(transaction, account.entity);
+    var app = new MoneyApp();
+    var account = getAccount(app, { entity_id: ENTITY_ID, account_id: INCOME_TAX_ID }, function(account) {
+      var transaction = {
+        transaction_date: "2014-02-27",
+        description: 'Paycheck', 
+        items: [
+          { account_id: CHECKING_ID, action: 'debit', amount: 1000 },
+          { account_id: SALARY_ID, action: 'credit', amount: 1000 }
+        ]
+      };
+      var viewModel = new TransactionViewModel(transaction, account.entity);
 
-    equal(viewModel.creditAmount(), 1000, "creditAmount should be the sum of all the credit items.");
+      equal(viewModel.formattedCreditAmount(), '1,000.00', "creditAmount should be the sum of all the credit items.");
 
-    var item = _.find(viewModel.items(), function(item) { return item.account_id() == 2; });
-    item.amount(987);
+      var item = _.find(viewModel.items(), function(item) { return item.account_id() == SALARY_ID; });
+      item.amount(987);
 
-    equal(viewModel.creditAmount(), 987, "creditAmount should change when an item with a 'credit' action changes");
+      equal(viewModel.formattedCreditAmount(), '987.00', "creditAmount should change when an item with a 'credit' action changes");
 
-    start();
+      start();
+    });
   });
-});
-asyncTest("formattedCreditAmount", function() {
-  expect(2);
+  asyncTest("debitAmount", function() {
+    expect(2);
 
-  var app = new MoneyApp();
-  var account = getAccount(app, { entity_id: 10, account_id: 3 }, function(account) {
-    var transaction = {
-      transaction_date: "2014-02-27",
-      description: 'Paycheck', 
-      items: [
-        { account_id: 1, action: 'debit', amount: 1000 },
-        { account_id: 2, action: 'credit', amount: 1000 }
-      ]
-    };
-    var viewModel = new TransactionViewModel(transaction, account.entity);
+    var app = new MoneyApp();
+    var account = getAccount(app, { entity_id: ENTITY_ID, account_id: INCOME_TAX_ID }, function(account) {
+      var transaction = {
+        transaction_date: "2014-02-27",
+        description: 'Paycheck', 
+        items: [
+          { account_id: CHECKING_ID, action: 'debit', amount: 1000 },
+          { account_id: SALARY_ID, action: 'credit', amount: 1000 }
+        ]
+      };
+      var viewModel = new TransactionViewModel(transaction, account.entity);
 
-    equal(viewModel.formattedCreditAmount(), '1,000.00', "creditAmount should be the sum of all the credit items.");
+      equal(viewModel.debitAmount(), 1000, "debitAmount should be the sum of all the debit items.");
 
-    var item = _.find(viewModel.items(), function(item) { return item.account_id() == 2; });
-    item.amount(987);
+      var item = _.find(viewModel.items(), function(item) { return item.account_id() == CHECKING_ID; });
+      item.amount(123);
 
-    equal(viewModel.formattedCreditAmount(), '987.00', "creditAmount should change when an item with a 'credit' action changes");
+      equal(viewModel.debitAmount(), 123, "debitAmount should change when an item with a 'debit' action changes");
 
-    start();
+      start();
+    });
   });
-});
-asyncTest("debitAmount", function() {
-  expect(2);
+  asyncTest("formattedDebitAmount", function() {
+    expect(2);
 
-  var app = new MoneyApp();
-  var account = getAccount(app, { entity_id: 10, account_id: 3 }, function(account) {
-    var transaction = {
-      transaction_date: "2014-02-27",
-      description: 'Paycheck', 
-      items: [
-        { account_id: 1, action: 'debit', amount: 1000 },
-        { account_id: 2, action: 'credit', amount: 1000 }
-      ]
-    };
-    var viewModel = new TransactionViewModel(transaction, account.entity);
+    var app = new MoneyApp();
+    var account = getAccount(app, { entity_id: ENTITY_ID, account_id: INCOME_TAX_ID }, function(account) {
+      var transaction = {
+        transaction_date: "2014-02-27",
+        description: 'Paycheck', 
+        items: [
+          { account_id: CHECKING_ID, action: 'debit', amount: 1000 },
+          { account_id: SALARY_ID, action: 'credit', amount: 1000 }
+        ]
+      };
+      var viewModel = new TransactionViewModel(transaction, account.entity);
 
-    equal(viewModel.debitAmount(), 1000, "debitAmount should be the sum of all the debit items.");
+      equal(viewModel.formattedDebitAmount(), '1,000.00', "debitAmount should be the sum of all the debit items.");
 
-    var item = _.find(viewModel.items(), function(item) { return item.account_id() == 1; });
-    item.amount(123);
+      var item = _.find(viewModel.items(), function(item) { return item.account_id() == CHECKING_ID; });
+      item.amount(123);
 
-    equal(viewModel.debitAmount(), 123, "debitAmount should change when an item with a 'debit' action changes");
+      equal(viewModel.formattedDebitAmount(), '123.00', "debitAmount should change when an item with a 'debit' action changes");
 
-    start();
+      start();
+    });
   });
-});
-asyncTest("formattedDebitAmount", function() {
-  expect(2);
+  asyncTest("validation", function() {
+    expect(6);
 
-  var app = new MoneyApp();
-  var account = getAccount(app, { entity_id: 10, account_id: 3 }, function(account) {
-    var transaction = {
-      transaction_date: "2014-02-27",
-      description: 'Paycheck', 
-      items: [
-        { account_id: 1, action: 'debit', amount: 1000 },
-        { account_id: 2, action: 'credit', amount: 1000 }
-      ]
-    };
-    var viewModel = new TransactionViewModel(transaction, account.entity);
+    var app = new MoneyApp();
+    // I get the account here because the account list must be loaded for the transaction item to work
+    var account = getAccount(app, { entity_id: ENTITY_ID, account_id: INCOME_TAX_ID }, function(account) {
+      var transaction = {
+        transaction_date: "2014-02-27",
+        description: 'Paycheck', 
+        items: [
+          { account_id: CHECKING_ID, action: 'debit', amount: 1000 },
+          { account_id: SALARY_ID, action: 'credit', amount: 1000 }
+        ]
+      };
+      var viewModel = new TransactionViewModel(transaction, account.entity);
+      ok(viewModel.validate(), "The model should be valid with a transaction date, description, and balanced items.");
 
-    equal(viewModel.formattedDebitAmount(), '1,000.00', "debitAmount should be the sum of all the debit items.");
+      viewModel.transaction_date(null);
+      equal(viewModel.validate(), false, "The model should not be valid without a transaction date.");
 
-    var item = _.find(viewModel.items(), function(item) { return item.account_id() == 1; });
-    item.amount(123);
+      viewModel.transaction_date("not a date");
+      equal(viewModel.validate(), false, "The model should not be valid with an invalid transaction date.");
 
-    equal(viewModel.formattedDebitAmount(), '123.00', "debitAmount should change when an item with a 'debit' action changes");
+      viewModel.transaction_date(new Date());
 
-    start();
-  });
-});
-asyncTest("validation", function() {
-  expect(6);
+      viewModel.description(null);
+      equal(viewModel.validate(), false, "The model should not be valid without a description.");
+      viewModel.description('test');
 
-  var app = new MoneyApp();
-  // I get the account here because the account list must be loaded for the transaction item to work
-  var account = getAccount(app, { entity_id: 10, account_id: 3 }, function(account) {
-    var transaction = {
-      transaction_date: "2014-02-27",
-      description: 'Paycheck', 
-      items: [
-        { account_id: 1, action: 'debit', amount: 1000 },
-        { account_id: 2, action: 'credit', amount: 1000 }
-      ]
-    };
-    var viewModel = new TransactionViewModel(transaction, account.entity);
-    ok(viewModel.validate(), "The model should be valid with a transaction date, description, and balanced items.");
+      var newItem = viewModel.addTransactionItem();
+      newItem.account_id(INCOME_TAX_ID);
+      newItem.action('debit');
+      newItem.amount(100);
 
-    viewModel.transaction_date(null);
-    equal(viewModel.validate(), false, "The model should not be valid without a transaction date.");
+      equal(viewModel.validate(), false, "The model should not be valid if the sum of credits does not equal the sum of debits.");
 
-    viewModel.transaction_date("not a date");
-    equal(viewModel.validate(), false, "The model should not be valid with an invalid transaction date.");
+      var checkingItem = _.find(viewModel.items(), function(item) { return item.account_id() == CHECKING_ID});
+      checkingItem.amount(900);
 
-    viewModel.transaction_date(new Date());
+      ok(viewModel.validate(), "The model should be valid if the sum of credits and debits are the same.");
 
-    viewModel.description(null);
-    equal(viewModel.validate(), false, "The model should not be valid without a description.");
-    viewModel.description('test');
+      start();
+    });
+  })
+  asyncTest("addTransactionItem", function() {
+    expect(3);
 
-    var newItem = viewModel.addTransactionItem();
-    newItem.account_id(3);
-    newItem.action('debit');
-    newItem.amount(100);
-
-    equal(viewModel.validate(), false, "The model should not be valid if the sum of credits does not equal the sum of debits.");
-
-    var checkingItem = _.find(viewModel.items(), function(item) { return item.account_id() == 1});
-    checkingItem.amount(900);
-
-    ok(viewModel.validate(), "The model should be valid if the sum of credits and debits are the same.");
-
-    start();
-  });
-})
-asyncTest("addTransactionItem", function() {
-  expect(3);
-
-  var app = new MoneyApp();
-  getTransaction(app, { entity_id: 10, account_id: 1, transaction_item_id: 10001 }, function(transaction) {
-    ok(transaction.addTransactionItem, "The object should have a method called 'addTransactionItem'");
-    if (transaction.addTransactionItem) {
-      var before = transaction.items().length;
-      var newItem = transaction.addTransactionItem();
-      ok(newItem, "It should not return null.");
-      var after = transaction.items().length;
-      equal(after - before, 1, "It should add an item to the items collection.");
-    }
-    start()
-  });
-});
-asyncTest('newAttachment', function() {
-  expect(3);
-
-  var app = new MoneyApp();
-  getTransaction(app, { entity_id: 10, account_id: 1, transaction_item_id: 10001 }, function(transaction) {
-    ok(transaction.newAttachment, 'The object should have a method called "newAttachment"');
-    if (transaction.newAttachment) {
-    transaction.newAttachment();
-    ok(app.editAttachment, 'The application object should have a method called "editAttachment"');
-      if (app.editAttachment) {
-        ok(app.editAttachment(), 'The application object editAttachment method should not return null after calling newAttachment on a transaction.');
+    var app = new MoneyApp();
+    getTransaction(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID, transaction_item_id: 10001 }, function(transaction) {
+      ok(transaction.addTransactionItem, "The object should have a method called 'addTransactionItem'");
+      if (transaction.addTransactionItem) {
+        var before = transaction.items().length;
+        var newItem = transaction.addTransactionItem();
+        ok(newItem, "It should not return null.");
+        var after = transaction.items().length;
+        equal(after - before, CHECKING_ID, "It should add an item to the items collection.");
       }
-    }
-    start();
+      start()
+    });
   });
-});
+  asyncTest('newAttachment', function() {
+    expect(3);
+
+    var app = new MoneyApp();
+    getTransaction(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID, transaction_item_id: 10001 }, function(transaction) {
+      ok(transaction.newAttachment, 'The object should have a method called "newAttachment"');
+      if (transaction.newAttachment) {
+      transaction.newAttachment();
+      ok(app.editAttachment, 'The application object should have a method called "editAttachment"');
+        if (app.editAttachment) {
+          ok(app.editAttachment(), 'The application object editAttachment method should not return null after calling newAttachment on a transaction.');
+        }
+      }
+      start();
+    });
+  });
+})();
