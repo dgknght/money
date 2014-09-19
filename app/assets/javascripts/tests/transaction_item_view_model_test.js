25440bc717caf6598cbfa920114cd7e1997a8dc3
Doug Knight <dougbknight@gmail.com>
2014-09-19 09:02:24 -0500

    finished cleaning up transaction item view model tests

--- app/assets/javascripts/tests/transaction_item_view_model_test.js
+++ app/assets/javascripts/tests/transaction_item_view_model_test.js
@@ -1,6 +1,11 @@
 (function() {
 
 var ENTITY_ID = 10;
+var CHECKING_ID = 101;
+var SALARY_ID = 102;
+var TRANSACTION_ID = 1001;
+var TRANSACTION_ITEM_1_ID = 10001;
+var TRANSACTION_ITEM_2_ID = 10002;
 
 module('TransactionItemViewModel', {
   setup: function() {
@@ -13,8 +18,8 @@ module('TransactionItemViewModel', {
     $.mockjax({
       url: 'entities/' + ENTITY_ID + '/accounts.json',
       responseText: [
-        { id: 101, name: 'Checking', account_type: 'asset' },
-        { id: 102, name: 'Salary', account_type: 'income' }
+        { id: CHECKING_ID, name: 'Checking', account_type: 'asset' },
+        { id: SALARY_ID, name: 'Salary', account_type: 'income' }
       ]
     });
     $.mockjax({
@@ -23,23 +28,24 @@ module('TransactionItemViewModel', {
       ]
     });
     $.mockjax({
-      url: 'entities/' + ENTITY_ID + '/transactions.json?account_id=101',
+      url: 'entities/' + ENTITY_ID + '/transactions.json?account_id=' + CHECKING_ID,
       responseText: [
-        { 
-          id: 1001, 
+        {
+          id: TRANSACTION_ID,
           transaction_date: '2014-01-01',
           description: 'Salary',
           items: [
-            { id: 10001, account_id: 101, action: 'debit', amount: "1000" },
-            { id: 10002, account_id: 102, action: 'credit', amount: "1000" }
+            { id: TRANSACTION_ITEM_1_ID, account_id: CHECKING_ID, action: 'debit', amount: "1000" },
+            { id: TRANSACTION_ITEM_2_ID, account_id: SALARY_ID, action: 'credit', amount: "1000" }
           ]
         },
       ]
     });
     $.mockjax({
-      url: 'account/*/lots.json',
+      url: 'accounts/*/lots.json',
       responseText: []
     });
+    $.mockjaxSettings.throwUnmocked = true;
   },
   teardown: function() {
     $.mockjaxClear();
@@ -49,8 +55,8 @@ asyncTest("amount", function() {
   expect(1);
 
   var app = new MoneyApp();
-  getAccount(app, { entity_id: ENTITY_ID, account_id: 101 }, function(account) {
-    getFromLazyLoadedCollection(account, 'transaction_items', 10001, function(item) {
+  getAccount(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID }, function(account) {
+    getFromLazyLoadedCollection(account, 'transaction_items', TRANSACTION_ITEM_1_ID, function(item) {
       ok(_.isNumber(item.transaction_item.amount() + 1), "The amount should be a number");
       start();
     });
@@ -60,8 +66,8 @@ asyncTest("accountPath", function() {
   expect(2);
 
   var app = new MoneyApp();
-  getAccount(app, { entity_id: ENTITY_ID, account_id: 101 }, function(account) {
-    getFromLazyLoadedCollection(account, 'transaction_items', 10001, function(item) {
+  getAccount(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID }, function(account) {
+    getFromLazyLoadedCollection(account, 'transaction_items', TRANSACTION_ITEM_1_ID, function(item) {
       var transactionItem = item.transaction_item;
       ok(transactionItem.accountPath, "The object should have an accountPath property accessor");
       if (transactionItem.accountPath)
@@ -74,24 +80,24 @@ asyncTest("remove", function() {
   expect(4);
 
   var app = new MoneyApp();
-  getAccount(app, { entity_id: ENTITY_ID, account_id: 101 }, function(account) {
-    getFromLazyLoadedCollection(account, 'transaction_items', 10001, function(item) {
+  getAccount(app, { entity_id: ENTITY_ID, account_id: CHECKING_ID }, function(account) {
+    getFromLazyLoadedCollection(account, 'transaction_items', TRANSACTION_ITEM_1_ID, function(item) {
       var transaction = item.transaction_item.transaction;
       item.transaction_item.remove();
       equal(1, transaction.items().length, "The number of items in the transaction should decrease by one after remove.");
-      
+
       var isAbsent = _.every(transaction.items(), function(i) { return i.id() != item.id()});
       ok(isAbsent, "The item should not longer be in the transaction after remove.");
 
       equal(transaction.debitAmount(), 0, "The debit amount on the transaction should reflect the removed item.");
 
-      var expected = { 
-        id: 1001, 
-        transaction_date: '2014-01-01', 
+      var expected = {
+        id: TRANSACTION_ID,
+        transaction_date: '2014-01-01',
         description: 'Salary',
         items_attributes: [
-          { id: 10001, account_id: 101, action: 'debit', amount: 0, _destroy: 1 },
-          { id: 10002, account_id: 102, action: 'credit', amount: 1000 }
+          { id: TRANSACTION_ITEM_1_ID, account_id: CHECKING_ID, action: 'debit', amount: 0, _destroy: 1 },
+          { id: TRANSACTION_ITEM_2_ID, account_id: SALARY_ID, action: 'credit', amount: 1000 }
         ]
       };
       deepEqual(transaction.toJson(), expected, "The serialized transaction should reflect the removed item.");
