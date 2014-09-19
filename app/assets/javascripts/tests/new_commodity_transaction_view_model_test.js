1fdc5e68857d0ea92dbb31fd45ea52da1a20b3e1
Doug Knight <dougbknight@gmail.com>
2014-09-19 16:43:40 -0500

    [#75774040] working the new commodity transaction view model spec

--- app/assets/javascripts/tests/new_commodity_transaction_view_model_test.js
+++ app/assets/javascripts/tests/new_commodity_transaction_view_model_test.js
@@ -1,8 +1,25 @@
 (function() {
-  var ENTITY_ID = 1;
-  var ACCOUNT_ID = 2;
+  var ENTITY_ID = 295836;
+  var ACCOUNT_ID = 943725;
   module('NewCommodityTransactionViewModel', {
-    setup: function() {},
+    setup: function() {
+      $.mockjax({
+        url: 'entities.json',
+        responseText: [
+          { id: ENTITY_ID, name: 'Personal' }
+        ]
+      });
+      $.mockjax({
+        url: 'entities/' + ENTITY_ID + '/accounts.json',
+        responseText: [
+          { id: ACCOUNT_ID, name: 'Checking', account_type: 'asset', content_type: 'currency' }
+        ]
+      });
+      $.mockjax({
+        url: 'accounts/' + ACCOUNT_ID + '/lots.json',
+        responseText: []
+      });
+    },
     teardown: function() {}
   });
   asyncTest('transaction_date', function() {
@@ -10,7 +27,7 @@
     getAccount(new MoneyApp(), { entity_id: ENTITY_ID, account_id: ACCOUNT_ID }, function(account) {
       ok(account.newCommodityTransaction.transaction_date, 'The new holding instance should have a "transaction_date" method');
       if (account.newCommodityTransaction.transaction_date) {
-        equal(account.newCommodityTransaction.transaction_date().toLocaleDateString(), new Date().toLocaleDateString(), 'The "action" method should default to today');
+        equal(account.newCommodityTransaction.transaction_date().toLocaleDateString(), new Date().toLocaleDateString(), 'The "transaction_date" method should default to today');
       }
       start();
     });
@@ -52,6 +69,73 @@
       if (account.newCommodityTransaction.value) {
         equal(account.newCommodityTransaction.value(), null, 'The "value" method should default to null');
       }
+      start();
+    });
+  });
+  asyncTest('price', function() {
+    expect(2);
+    getAccount(new MoneyApp(), { entity_id: ENTITY_ID, account_id: ACCOUNT_ID }, function(account) {
+      ok(account.newCommodityTransaction.price, 'The instance should have a "price" method');
+      if (account.newCommodityTransaction.price) {
+        account.newCommodityTransaction.shares(100);
+        account.newCommodityTransaction.value(1200);
+        equal(account.newCommodityTransaction.price(), 12, 'The "price" method should return the correct value');
+      }
+      start();
+    });
+  });
+  asyncTest('validate', function() {
+    getAccount(new MoneyApp(), { entity_id: ENTITY_ID, account_id: ACCOUNT_ID }, function(account) {
+      ok(account.newCommodityTransaction.validate, 'The instance should have a "validate" method');
+      if (account.newCommodityTransaction.validate) {
+        var viewModel = account.newCommodityTransaction;
+
+        viewModel.transaction_date(new Date(2014, 1, 27));
+        viewModel.symbol('KSS');
+        viewModel.shares(100);
+        viewModel.value(1200);
+        ok(viewModel.validate(), 'The view model should be valid with a transaction_date, symbol, shares, and value');
+
+        // action
+        viewModel.action(null);
+        equal(viewModel.validate(), false, '"action" should be required');
+
+        viewModel.action('not_a_valid_action');
+        equal(viewModel.validate(), false, '"action" must be "buy" or "sell"');
+        equal(viewModel.action.errorMessage(), 'The value must be one of these values: buy, sell.', "The property should have the correct error message");
+
+        viewModel.action('buy');
+
+        // symbol
+        viewModel.symbol(null);
+        equal(viewModel.validate(), false, '"symbol" should be required');
+
+        viewModel.symbol('KSS');
+
+
+        // shares
+        viewModel.shares(null);
+        equal(viewModel.validate(), false, '"shares" should be required');
+        viewModel.shares('not_a_number');
+        equal(viewModel.validate(), false, '"shares" should be a number');
+
+        viewModel.shares(100);
+
+        // value
+        viewModel.value(null);
+        equal(viewModel.validate(), false, '"value" should be required');
+        viewModel.value('not_a_number');
+        equal(viewModel.validate(), false, '"value" should be a number');
+
+        viewModel.value(1200);
+
+        // transaction_date
+//        viewModel.formattedTransactionDate(null);
+//        equal(viewModel.validate(), false, '"transaction_date" should be required');
+//        viewModel.formattedTransactionDate('not_a_date');
+//        equal(viewModel.validate(), false, '"transaction_date" should be a date');
+      }
+
       start();
     });
   });
--- app/assets/javascripts/view_models/new_commodity_transaction_view_model.js
+++ app/assets/javascripts/view_models/new_commodity_transaction_view_model.js
@@ -5,6 +5,31 @@ function NewCommodityTransactionViewModel(account) {
   var _self = this;
   this._account = account;
 
-  this.action = ko.observable('buy');
+  this.action = ko.observable('buy').extend({ required: "You must select an action.", includedIn: ['buy', 'sell']  });
   this.transaction_date = ko.observable(new Date());
+  this.symbol = ko.observable().extend({ required: "You must enter a valid commodity symbol." });
+  this.shares = ko.observable().extend({ required: "You must enter a valid number of shares.", numeric: '"Shares" must be a valid number.' });
+  this.value = ko.observable().extend({ required: "You must enter a valid numeric 'value'.", numeric: '"Value" must be a valid number.' });
+
+  this.formattedTransactionDate = ko.computed({
+    read: function() {
+      return _self.transaction_date() ?  _self.transaction_date().toLocaleDateString() : null;
+    },
+    write: function(value) {
+      var dateValue = new Date(value);
+      if (!isNaN(dateValue))
+        _self.transaction_date(dateValue);
+    }
+  });
+
+  this.price = ko.computed(function() {
+    if (this.value() && this.shares())
+      return this.value() / this.shares();
+    return null;
+  }, this);
+
+  this.validate = function() {
+    var props = [_self.action, _self.transaction_date, _self.symbol, _self.shares, _self.value];
+    return _.every(props, function(prop) { return prop.hasError == null || !prop.hasError(); });
+  };
 }
