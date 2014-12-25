d5c54f1f77ce8ba7ac766bcca684912291fd8fc8
Doug Knight <administrator@knightsoftwareservices.com>
2014-12-25 12:11:15 -0600

    fixed misc problems with datepickers, autofocus on year in budget forms

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,6 +1,5 @@
 $(function() {
-  // TODO Need to figure out why this is breaking the single page date pickers
-//  $('.date_field').datepicker();
+  $('.date_field').datepicker();
 });
 
 $(function() {
