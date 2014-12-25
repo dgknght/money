913fdda4c0bd2073d179434b3b0580a9db9cd92b
Doug Knight <administrator@knightsoftwareservices.com>
2014-12-25 10:21:34 -0600

    [#85146120] separated the ajax navigation from the plain html navigation

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,3 +2,9 @@ $(function() {
   // TODO Need to figure out why this is breaking the single page date pickers
 //  $('.date_field').datepicker();
 });
+
+$(function() {
+  // If javascript is enabled, hide the HTML only nav and show the ajax nav
+  $('#html_nav').hide();
+  $('#ajax_nav').show();
+});
