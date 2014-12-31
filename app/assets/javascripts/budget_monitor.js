d12891b948dc29b8ac0e5be923f4ad20dd64935a
Doug Knight <administrator@knightsoftwareservices.com>
2014-12-30 20:54:06 -0600

    refined the budget monitor widget

--- app/assets/javascripts/budget_monitor.js
+++ app/assets/javascripts/budget_monitor.js
@@ -11,32 +11,67 @@ $.widget("dgknght.budget_monitor", {
   _create: function() {
     var c = document.createElement("canvas");
     this.element.append(c);
+    c.width = 256;
+    c.height = 32;
     var ctx = c.getContext("2d");
+    var padding = 4;
 
-    var width = 300; //TODO Read this from the element? The options?
-    var height = 50; 
+    var width = c.width - (2 * padding);
+    var height = c.height - (2 * padding);
+    var lineWidth = 4;
+
+    var budget = parseInt(this.element.data("budget"));
+    var current = parseInt(this.element.data("current"));
+    var percentOfBudgetSpent = current / budget;
+    if (percentOfBudgetSpent > 1) percentOfBudgetSpent = 1;
+
+    var daysAvailable = this._daysAvailable();
+    var daysPast = this._daysPast();
+
+    var percentOfTimePast = (daysPast / daysAvailable);
+    var paceAmount = percentOfTimePast * budget;
+
+    // inner fill
+    ctx.fillStyle = paceAmount >= current ? this._safeFillColor() : this._dangerFillColor();
+    ctx.fillRect(padding, padding, percentOfBudgetSpent * width, height);
 
     // outer box
     ctx.strokeStyle = "#000";
     ctx.lineWidth = 2;
-    ctx.strokeRect(0, 0, width, height);
-
-    // inner fill
-    var budget = parseInt(this.element.data("budget"));
-    var current = parseInt(this.element.data("current"));
-    ctx.fillStyle = "#00F";
-    ctx.fillRect(2, 2, (current / budget) * (width-2), (height-2));
+    ctx.strokeRect(padding, padding, width, height);
     
     // pace line
-    var daysAvailable = parseInt(this.element.data("days-available"));
-    var daysPast = parseInt(this.element.data("days-past"));
-    var paceX = (daysPast / daysAvailable) * (width-2);
+    var paceX = (daysPast / daysAvailable) * width;
 
     ctx.beginPath();
-    ctx.moveTo(paceX, 2);
-    ctx.lineTo(paceX, height-2);
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = "#F99";
+    ctx.moveTo(paceX, 0);
+    ctx.lineTo(paceX, c.height);
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = "#33F";
     ctx.stroke();
+  },
+
+  _daysAvailable: function() {
+    if (this.options.daysAvailable)
+      return parseInt(this.options.daysAvailable);
+
+    var now = new Date();
+    year = now.getFullYear();
+    month = now.getMonth() + 1;
+    return new Date(year, month, 0);
+  },
+
+  _daysPast: function() {
+    if (this.options.daysPast)
+      return parseInt(this.options.daysPast);
+    return new Date().getDate();
+  },
+
+  _safeFillColor: function() {
+    return this.options.safeFillColor || "#3F3";
+  },
+
+  _dangerFillColor: function() {
+    return this.options.dangerFillColor || "#F33";
   }
 });
