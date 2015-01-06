0e6de1ad14f575a55f65722f64cbd264b8409221
Doug Knight <dougbknight@gmail.com>
2015-01-05 21:45:39 -0600

    [#85334100] added BudgetMonitor#budget_amount spec (WIP)

--- app/models/budget_monitor.rb
+++ app/models/budget_monitor.rb
@@ -5,10 +5,25 @@ class BudgetMonitor < ActiveRecord::Base
   validates_presence_of :account_id, :entity_id
 
   def budget_amount
-    100
+
+    puts "budgets=#{Budget.all.inspect}"
+    puts "today is #{Date.today}"
+
+    budget = Budget.current
+    budget_item = budget.item_for(account)
+    period = budget_item.current_period
+    period.budget_amount * progress
   end
 
   def current_amount
     50
+  end
+
+  private
+
+  def progress
+    today = Date.today
+    days_in_month = Date.days_in_month(today.year, today.month)
+    today.day / days_in_month
   end
 end
--- spec/models/budget_monitor_spec.rb
+++ spec/models/budget_monitor_spec.rb
@@ -21,4 +21,16 @@ describe BudgetMonitor do
       expect(monitor).to have(1).error_on(:account_id)
     end
   end
+
+  describe '#budget_amount' do
+    let (:budget) { FactoryGirl.create(:budget, entity: entity, start_date: Date.parse('2015-01-01')) }
+    let (:budget_monitor) { FactoryGirl.create(:budget_monitor, account: account, entity: entity) }
+    let (:budget_item) { FactoryGirl.create(:budget_item, budget: budget, account: account) }
+    let!(:budget_item_period) { FactoryGirl.create(:budget_item_period, budget_item: budget_item, start_date: Date.parse('2015-02-01'), budget_amount: 500) }
+    it 'should return the amount budget for the period, prorated for the number of days that have passed' do
+      Timecop.freeze(Date.parse('2015-02-14')) do
+        expect(budget_monitor.budget_amount).to eq(250)
+      end
+    end
+  end
 end
