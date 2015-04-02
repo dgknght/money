50f40b1e87d7aa62d389b446e55f58b2af001c8a
Doug Knight <dougbknight@gmail.com>
2015-04-02 18:13:32 -0500

    [#91331270] Corrected the order of items in the balance sheet test for the gnucash importer

--- spec/models/gnucash_importer_spec.rb
+++ spec/models/gnucash_importer_spec.rb
@@ -80,23 +80,23 @@ describe GnucashImporter do
 
     it 'should result in a balance sheet report with correct balances' do
       GnucashImporter.new(attributes).import!
-      report = BalanceSheetReport.new(entity)
+      report = BalanceSheetReport.new(entity, BalanceSheetFilter.new(as_of: '2015-02-28'))
       expected = [{account: "Assets"              , balance: "249,721.00", depth: 0},
                   {account: "Current Assets"      , balance:   "2,688.00", depth: 1},
                   {account: "Checking"            , balance:   "2,688.00", depth: 2},
                   {account: "Fixed Assets"        , balance: "225,000.00", depth: 1},
                   {account: "House"               , balance: "200,000.00", depth: 2},
                   {account: "Vehicle"             , balance:  "25,000.00", depth: 2},
+                  {account: "Imbalance-USD"       , balance:       "0.00", depth: 1},
                   {account: "Investments"         , balance:  "22,033.00", depth: 1},
                   {account: "401k"                , balance:  "22,033.00", depth: 2},
-                  {account: "Imbalance-USD"       , balance:       "0.00", depth: 1},
                   {account: "Liabilities"         , balance:  "24,400.00", depth: 0},
                   {account: "Loans"               , balance:  "24,400.00", depth: 1},
                   {account: "Vehicle Loan"        , balance:  "24,400.00", depth: 2},
                   {account: "Equity"              , balance: "225,321.00", depth: 0},
                   {account: "Opening Balances"    , balance: "220,000.00", depth: 1},
-                  {account: "Unrealized Gains"    , balance:   "2,033.00", depth: 1},
                   {account: "Retained Earnings"   , balance:   "3,288.00", depth: 1},
+                  {account: "Unrealized Gains"    , balance:   "2,033.00", depth: 1},
                   {account: "Liabilities + Equity", balance: "249,721.00", depth: 0}]
       expect(report.content).to eq(expected)
     end
