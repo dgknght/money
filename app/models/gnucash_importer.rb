e5984259a9135ec2d1fbe5ffc295362713e5bd78
Doug Knight <dougbknight@gmail.com>
2015-04-19 18:18:35 -0500

    process prices

--- app/models/gnucash_importer.rb
+++ app/models/gnucash_importer.rb
@@ -12,7 +12,7 @@ class GnucashImporter
     "MUTUAL" => Account.commodity_content}
   IGNORE_ACCOUNTS = ["Root Account", "Assets", "Liabilities", "Expenses", "Income", "Equity"]
 
-  ELEMENTS_TO_PROCESS = ["gnc:account", "gnc:commodity"]
+  ELEMENTS_TO_PROCESS = ["gnc:account", "gnc:commodity", "price"]
 
   attr_accessor :data, :entity
   validates_presence_of :data, :entity
@@ -74,6 +74,16 @@ class GnucashImporter
     TYPE_MAP.fetch(type, type.downcase)
   end
 
+  # The amount comes in as a fraction, like 200000/100
+  def parse_amount(input)
+    numerator, denominator = input.split('/')
+    BigDecimal.new(numerator) / BigDecimal.new(denominator)
+  end
+
+  def prices_read
+    @prices_read ||= Set.new
+  end
+
   def process_account_element(source)
     return if ignore_account?(source["act:name"])
 
@@ -113,6 +123,29 @@ class GnucashImporter
     else
       puts "Unrecognized element #{name}, #{elem.inspect}"
     end
+  end
+
+  def process_price_element(source)
+    amount = parse_amount(source["price:value"])
+    return unless amount > 0
+
+    symbol = source["price:commodity"]["cmdty:id"]
+    trade_date = source["price:time"]["ts:date"].to_date
+    key = "#{symbol}:#{trade_date}"
+    unless prices_read.include?(key)
+      commodity = @commodities[symbol]
+      price = commodity.prices.new(trade_date: trade_date,
+                                   price: amount)
+
+      if price.save(validate: false) # skip validation for performance reasons
+        prices_read << key
+        @trace_method.call 'p'
+      else
+        Rails.logger.warn "Unable to import the price.\n  source=#{source.inspect}\n  #{price.errors.full_messages.to_sentence}"
+      end
+    end
+  rescue StandardError => e
+    Rails.logger.warn "Unable to import the price.\n  source=#{source.inspect}\n  #{e.message}\n  #{e.backtrace.join("\n    ")}"
   end
 
   def gzip_reader
