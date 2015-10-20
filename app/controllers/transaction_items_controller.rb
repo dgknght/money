da322595ce1134eb75d6af9457cb3496422bcb76
Doug Knight <dougbknight@gmail.com>
2015-10-20 12:11:06 -0500

    use Account#transaction_items instead of #transaction_items_backwards

--- app/controllers/transaction_items_controller.rb
+++ app/controllers/transaction_items_controller.rb
@@ -41,7 +41,7 @@ class TransactionItemsController < ApplicationController
   
   def index
     authorize! :show, @account
-    @transaction_items = @account.transaction_items_backward.take(5).to_a
+    @transaction_items = @account.transaction_items.order('transaction_items."index" desc').take(10).to_a
     respond_with @transaction_items
   end
   
