ce0be37d23169c3cb1cd141a1307b79bfefd820d
Doug Knight <dougbknight@gmail.com>
2015-11-02 15:39:47 -0600

    remove plpgsql line from schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151021155753) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: true do |t|
     t.string   "name",                                      null: false
     t.string   "account_type",                              null: false
