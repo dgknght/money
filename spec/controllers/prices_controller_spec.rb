3f6161b7446ebb7cce30dc833af4e321666124bf
Doug Knight <dougbknight@gmail.com>
2015-11-06 22:51:36 -0600

    correct 'should' phrasing in spec

--- spec/controllers/prices_controller_spec.rb
+++ spec/controllers/prices_controller_spec.rb
@@ -17,19 +17,19 @@ describe PricesController do
       before(:each) { sign_in entity.user }
 
       describe 'get :index' do
-        it 'should be successful' do
+        it 'is successful' do
           get :index, commodity_id: commodity
           expect(response).to be_success
         end
 
         context 'in json' do
           let!(:price_2) { FactoryGirl.create(:price, commodity: commodity, trade_date: '2014-03-01') }
-          it 'should be successful' do
+          it 'is successful' do
             get :index, commodity_id: commodity, format: :json
             expect(response).to be_success
           end
 
-          it 'should return the list of prices' do
+          it 'returns the list of prices' do
             get :index, commodity_id: commodity, format: :json
             expect(response.body).to json_match([price_2, price])
           end
@@ -37,18 +37,18 @@ describe PricesController do
       end
 
       describe 'get :show' do
-        it 'should be successful' do
+        it 'is successful' do
           get :show, id: price
           expect(response).to be_success
         end
 
         context 'in json' do
-          it 'should be successful' do
+          it 'is successful' do
             get :show, id: price, format: :json
             expect(response).to be_success
           end
 
-          it 'should return the specified price' do
+          it 'returns the specified price' do
             get :show, id: price, format: :json
             expect(response.body).to json_match(price)
           end
@@ -56,37 +56,37 @@ describe PricesController do
       end
 
       describe 'get :new' do
-        it 'should be successful' do
+        it 'is successful' do
           get :new, commodity_id: commodity
           expect(response).to be_success
         end
       end
 
       describe 'post :create' do
-        it 'should redirect to the price index page' do
+        it 'redirects to the price index page' do
           post :create, commodity_id: commodity, price: attributes
           expect(response).to redirect_to commodity_prices_path(commodity)
         end
 
-        it 'should create a new price' do
+        it 'creates a new price' do
           expect do
             post :create, commodity_id: commodity, price: attributes
           end.to change(commodity.prices, :count).by(1)
         end
 
         context 'in json' do
-          it 'should be successful' do
+          it 'is successful' do
             post :create, commodity_id: commodity, price: attributes, format: :json
             expect(response).to be_success
           end
 
-          it 'should create a new price' do
+          it 'creates a new price' do
             expect do
               post :create, commodity_id: commodity, price: attributes, format: :json
             end.to change(commodity.prices, :count).by(1)
           end
 
-          it 'should return the new price' do
+          it 'returns the new price' do
             post :create, commodity_id: commodity, price: attributes, format: :json
             returned = JSON.parse(response.body)
             attributes.each do |key, value|
@@ -97,19 +97,19 @@ describe PricesController do
       end
 
       describe 'get :edit' do
-        it 'should be successful' do
+        it 'is successful' do
           get :edit, id: price
           expect(response).to be_success
         end
       end
 
       describe 'put :update' do
-        it 'should redirect to the prices index page' do
+        it 'redirects to the prices index page' do
           put :update, id: price, price: attributes
           expect(response).to redirect_to commodity_prices_path(commodity)
         end
 
-        it 'should update the specified price' do
+        it 'updates the specified price' do
           expect do
             put :update, id: price, price: attributes
             price.reload
@@ -117,12 +117,12 @@ describe PricesController do
         end
 
         context 'in json' do
-          it 'should be successful' do
+          it 'is successful' do
             put :update, id: price, price: attributes, format: :json
             expect(response).to be_success
           end
 
-          it 'should update the specified price' do
+          it 'updates the specified price' do
             expect do
               put :update, id: price, price: attributes, format: :json
               price.reload
@@ -132,24 +132,24 @@ describe PricesController do
       end
 
       describe 'delete :destroy' do
-        it 'should redirect to the prices index page' do
+        it 'redirects to the prices index page' do
           delete :destroy, id: price
           expect(response).to redirect_to commodity_prices_path(commodity)
         end
 
-        it 'should delete the specified price' do
+        it 'deletes the specified price' do
           expect do
             delete :destroy, id: price
           end.to change(Price, :count).by(-1)
         end
 
         context 'in json' do
-          it 'should be successful' do
+          it 'is successful' do
             delete :destroy, id: price, format: :json
             expect(response).to be_success
           end
 
-          it 'should delete the specified price' do
+          it 'deletes the specified price' do
             expect do
               delete :destroy, id: price, format: :json
             end.to change(Price, :count).by(-1)
@@ -163,7 +163,7 @@ describe PricesController do
           expect(response).to redirect_to entity_commodities_path(entity)
         end
 
-        it 'should initiate a price download' do
+        it 'initiates a price download' do
           allow(StockPrices::PriceDownloader).to receive(:download)
           patch :download, entity_id: entity
         end
@@ -175,18 +175,18 @@ describe PricesController do
       before(:each) { sign_in other_user }
 
       describe 'get :index' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           get :index, commodity_id: commodity
           expect(response).to redirect_to home_path
         end
 
         context 'in json' do
-          it 'should return "resource not found"' do
+          it 'returns "resource not found"' do
             get :index, commodity_id: commodity, format: :json
             expect(response.response_code).to eq(404)
           end
 
-          it 'should not return any data' do
+          it 'does not return any data' do
             get :index, commodity_id: commodity, format: :json
             expect(response.body).to eq([].to_json)
           end
@@ -194,18 +194,18 @@ describe PricesController do
       end
 
       describe 'get :show' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           get :show, id: price
           expect(response).to redirect_to home_path
         end
 
         context 'in json' do
-          it 'should return "resource not found"' do
+          it 'returns "resource not found"' do
             get :show, id: price, format: :json
             expect(response.response_code).to eq(404)
           end
 
-          it 'should not return any data' do
+          it 'does not return any data' do
             get :show, id: price, format: :json
             expect(response.body).to eq([].to_json)
           end
@@ -213,37 +213,37 @@ describe PricesController do
       end
 
       describe 'get :new' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           get :new, commodity_id: commodity
           expect(response).to redirect_to home_path
         end
       end
 
       describe 'post :create' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           post :create, commodity_id: commodity, price: attributes
           expect(response).to redirect_to home_path
         end
 
-        it 'should not create a new price' do
+        it 'does not create a new price' do
           expect do
             post :create, commodity_id: commodity, price: attributes
           end.not_to change(Price, :count)
         end
 
         context 'in json' do
-          it 'should return "resource not found"' do
+          it 'returns "resource not found"' do
             post :create, commodity_id: commodity, price: attributes, format: :json
             expect(response.response_code).to eq(404)
           end
 
-          it 'should not create a new price' do
+          it 'does not create a new price' do
             expect do
               post :create, commodity_id: commodity, price: attributes, format: :json
             end.not_to change(Price, :count)
           end
 
-          it 'should not return any data' do
+          it 'does not return any data' do
             post :create, commodity_id: commodity, price: attributes, format: :json
             expect(response.body).to eq([].to_json)
           end
@@ -251,19 +251,19 @@ describe PricesController do
       end
 
       describe 'get :edit' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           get :edit, id: price
           expect(response).to redirect_to home_path
         end
       end
 
       describe 'put :update' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           put :update, id: price, price: attributes
           expect(response).to redirect_to home_path
         end
 
-        it 'should not update the specified price' do
+        it 'does not update the specified price' do
           expect do
             put :update, id: price, price: attributes
             price.reload
@@ -271,19 +271,19 @@ describe PricesController do
         end
 
         context 'in json' do
-          it 'should return "resource not found"' do
+          it 'returns "resource not found"' do
             put :update, id: price, price: attributes, format: :json
             expect(response.response_code).to eq(404)
           end
 
-          it 'should not update the specified price' do
+          it 'does not update the specified price' do
             expect do
               put :update, id: price, price: attributes, format: :json
               price.reload
             end.not_to change(price, :price)
           end
 
-          it 'should not return any data' do
+          it 'does not return any data' do
             put :update, id: price, price: attributes, format: :json
             expect(response.body).to eq([].to_json)
           end
@@ -291,24 +291,24 @@ describe PricesController do
       end
 
       describe 'delete :destroy' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           delete :destroy, id: price
           expect(response).to redirect_to home_path
         end
 
-        it 'should not delete the specified price' do
+        it 'does not delete the specified price' do
           expect do
             delete :destroy, id: price
           end.not_to change(Price, :count)
         end
 
         context 'in json' do
-          it 'should return "resource not found"' do
+          it 'returns "resource not found"' do
             delete :destroy, id: price, format: :json
             expect(response.response_code).to eq(404)
           end
 
-          it 'should not delete the specified price' do
+          it 'does not delete the specified price' do
             expect do
               delete :destroy, id: price, format: :json
             end.not_to change(Price, :count)
@@ -317,12 +317,12 @@ describe PricesController do
       end
 
       describe 'patch :download' do
-        it 'should redirect to the user home page' do
+        it 'redirects to the user home page' do
           patch :download, entity_id: entity
           expect(response).to redirect_to(home_path)
         end
 
-        it 'should not initiate a price download' do
+        it 'does not initiate a price download' do
           allow(StockPrices::PriceDownloader).to receive(:download).never
           patch :download, entity_id: entity
         end
@@ -332,18 +332,18 @@ describe PricesController do
 
   context 'for an unauthenticated user' do
     describe 'get :index' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         get :index, commodity_id: commodity
         expect(response).to redirect_to(new_user_session_path)
       end
 
       context 'in json' do
-        it 'should return "access denied"' do
+        it 'returns "access denied"' do
           get :index, commodity_id: commodity, format: :json
           expect(response.response_code).to eq(401)
         end
 
-        it 'should not return any data' do
+        it 'does not return any data' do
           get :index, commodity_id: commodity, format: :json
           expect(JSON.parse(response.body)).to have_only('error')
         end
@@ -351,18 +351,18 @@ describe PricesController do
     end
 
     describe 'get :show' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         get :show, id: price
         expect(response).to redirect_to new_user_session_path
       end
 
       context 'in json' do
-        it 'should return "access denied"' do
+        it 'returns "access denied"' do
           get :show, id: price, format: :json
           expect(response.response_code).to eq(401)
         end
 
-        it 'should not return any data' do
+        it 'does not return any data' do
           get :show, id: price, format: :json
           expect(JSON.parse(response.body)).to have_only('error')
         end
@@ -370,37 +370,37 @@ describe PricesController do
     end
 
     describe 'get :new' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         get :new, commodity_id: commodity
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     describe 'post :create' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         post :create, commodity_id: commodity
         expect(response).to redirect_to new_user_session_path
       end
 
-      it 'should not create a new price' do
+      it 'does not create a new price' do
         expect do
           post :create, commodity_id: commodity
         end.not_to change(Price, :count)
       end
 
       context 'in json' do
-        it 'should return "access denied"' do
+        it 'returns "access denied"' do
           post :create, commodity_id: commodity, format: :json
           expect(response.response_code).to eq(401)
         end
 
-        it 'should not create a new price' do
+        it 'does not create a new price' do
           expect do
             post :create, commodity_id: commodity, format: :json
           end.not_to change(Price, :count)
         end
 
-        it 'should not return any data' do
+        it 'does not return any data' do
           post :create, commodity_id: commodity, format: :json
           expect(JSON.parse(response.body)).to have_only('error')
         end
@@ -408,19 +408,19 @@ describe PricesController do
     end
 
     describe 'get :edit' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         get :edit, id: price
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     describe 'put :update' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         put :update, id: price, price: attributes
         expect(response).to redirect_to new_user_session_path
       end
 
-      it 'should not update the specified price' do
+      it 'does not update the specified price' do
         expect do
           put :update, id: price, price: attributes
           price.reload
@@ -428,12 +428,12 @@ describe PricesController do
       end
 
       context 'in json' do
-        it 'should return "access denied"' do
+        it 'returns "access denied"' do
           put :update, id: price, price: attributes, format: :json
           expect(response.response_code).to eq(401)
         end
 
-        it 'should not update the specified price' do
+        it 'does not update the specified price' do
           expect do
             put :update, id: price, price: attributes, format: :json
             price.reload
@@ -443,24 +443,24 @@ describe PricesController do
     end
 
     describe 'delete :destroy' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         delete :destroy, id: price
         expect(response).to redirect_to new_user_session_path
       end
 
-      it 'should not delete the specified price' do
+      it 'does not delete the specified price' do
         expect do
           delete :destroy, id: price
         end.not_to change(Price, :count)
       end
 
       context 'in json' do
-        it 'should return "access denied"' do
+        it 'returns "access denied"' do
           delete :destroy, id: price, format: :json
           expect(response.response_code).to eq(401)
         end
 
-        it 'should not delete the specified price' do
+        it 'does not delete the specified price' do
           expect do
             delete :destroy, id: price, format: :json
           end.not_to change(Price, :count)
@@ -469,12 +469,12 @@ describe PricesController do
     end
 
     describe 'patch :download' do
-      it 'should redirect to the sign in page' do
+      it 'redirects to the sign in page' do
         patch :download, entity_id: entity
         expect(response).to redirect_to(new_user_session_path)
       end
 
-      it 'should not initiate a price download' do
+      it 'does not initiate a price download' do
         allow(StockPrices::PriceDownloader).to receive(:download).never
         patch :download, entity_id: entity
       end
