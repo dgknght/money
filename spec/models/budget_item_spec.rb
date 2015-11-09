be89c5c190743e10e6447305b2560c7586b8081c
Doug Knight <dougbknight@gmail.com>
2015-11-09 12:25:35 -0600

    should -> expect

--- spec/models/budget_item_spec.rb
+++ spec/models/budget_item_spec.rb
@@ -8,86 +8,86 @@ describe BudgetItem do
   let(:attributes) do
     {
       budget_id: budget.id,
-      account_id: groceries.id      
+      account_id: groceries.id
     }
   end
-  
-  it 'should be creatable from valid attributes' do
+
+  it 'is creatable from valid attributes' do
     item = BudgetItem.new(attributes)
-    item.should be_valid
+    expect(item).to be_valid
   end
-  
+
   describe 'budget_id' do
-    it 'should be required' do
+    it 'is required' do
       item = BudgetItem.new(attributes.without(:budget_id))
-      item.should_not be_valid
+      expect(item).to_not be_valid
     end
   end
-  
+
   describe 'budget' do
-    it 'should reference the budget to which the item belongs' do
+    it 'references the budget to which the item belongs' do
       item = BudgetItem.new(attributes)
-      item.budget.should == budget
+      expect(item.budget).to eq(budget)
     end
   end
-  
+
   describe 'account_id' do
-    it 'should be required' do
+    it 'is required' do
       item = BudgetItem.new(attributes.without(:account_id))
-      item.should_not be_valid      
+      expect(item).to_not be_valid
     end
-    
-    it 'should be unique within a given budget' do
+
+    it 'is unique within a given budget' do
       item = BudgetItem.create!(attributes)
       item2 = BudgetItem.new(attributes)
-      item.should be_valid
-      item2.should_not be_valid
+      expect(item).to be_valid
+      expect(item2).to_not be_valid
     end
   end
-  
+
   describe 'account' do
-    it 'should reference the account for which amounts are specified' do
+    it 'references the account for which amounts are specified' do
       item = BudgetItem.new(attributes)
-      item.account.should == groceries
+      expect(item.account).to eq(groceries)
     end
   end
-  
+
   describe 'periods' do
-    it 'should contain a list of the periods within the budget' do
+    it 'contains a list of the periods within the budget' do
       item = budget.items.create(attributes)
-      item.periods.should_not be_nil
+      expect(item.periods).to_not be_nil
       start_dates = (1..12).map{|m| Date.parse("2015-%02d-01" % m)}
       expect(item.periods.map{|p| p.start_date}).to eq(start_dates)
     end
   end
-  
+
   describe 'scope' do
     let!(:salary_item) { FactoryGirl.create(:budget_item, budget: budget, account: salary) }
     let!(:groceries_item) { FactoryGirl.create(:budget_item, budget: budget, account: groceries) }
-    
+
     describe 'income' do
-      it 'should return budget items for income accounts' do
-        budget.items.income.should == [salary_item]
+      it 'returns budget items for income accounts' do
+        expect(budget.items.income).to eq([salary_item])
       end
     end
-    
+
     describe 'expense' do
-      it 'should return budget items for expense accounts' do
-        budget.items.expense.should == [groceries_item]
+      it 'returns budget items for expense accounts' do
+        expect(budget.items.expense).to eq([groceries_item])
       end
     end
   end
 
   describe '#current_period' do
     let (:budget_item) { FactoryGirl.create(:budget_item, budget: budget, account: groceries) }
-    it 'should return the period within the budget item in which the current date falls' do
+    it 'returns the period within the budget item in which the current date falls' do
       Timecop.freeze(Date.parse('2015-02-27')) do
         expect(budget_item.current_period).not_to be_nil
         expect(budget_item.current_period.start_date).to eq(Date.parse('2015-02-01'))
       end
     end
 
-    it 'should return nil if the budget to which the item belongs is not current' do
+    it 'returns nil if the budget to which the item belongs is not current' do
       Timecop.freeze(Date.parse('2001-02-27')) do
         expect(budget_item.current_period).to be_nil
       end
