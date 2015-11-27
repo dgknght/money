dd0187f15fb37ea48de12f4b40b947d6b6e41aa5
Doug Knight <dougbknight@gmail.com>
2015-11-27 15:47:39 -0600

    add missing javascript refs to non-ajax app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-2.0.3.min
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
 //= require common
+//= require bootstrap.min
+//= require ui-bootstrap-tpls-0.14.3.min
