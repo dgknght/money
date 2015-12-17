9ce9be3f58a7fe5091079ccccffb6d01e2f9a5ff
Doug Knight <dougbknight@gmail.com>
2015-12-17 09:59:29 -0600

    refine cache_in behavior

--- lib/cache_in/object.rb
+++ lib/cache_in/object.rb
@@ -1,20 +1,31 @@
 class Object
+
+  DEFAULT_MEMOIZE_OPTIONS = {ttl: 15.minutes}
+
   def memo_store
     @memo_store ||= {}
   end
 
-  def self.memoize(method, ttl = nil)
-    old_method = "orig_#{method}".to_sym
-    alias_method old_method, method
-    define_method method do |*args|
-      key = "method_#{args.map(&:to_s).join("_")}"
-      stored_value = memo_store[key]
-      unless stored_value && stored_value[:expiration] > DateTime.now
-        raw_result = send(old_method, *args)
-        stored_value = {value: raw_result, expiration: DateTime.now + 5.minutes}
-        memo_store[key] = stored_value
-      end
-      stored_value[:value]
+  class << self
+    def memoize(methods, options = {})
+      options = DEFAULT_MEMOIZE_OPTIONS.merge(options || {})
+      Array(methods).each{|m| _memoize m, options}
     end
+
+    def _memoize(method, options)
+      old_method = "orig_#{method}".to_sym
+      alias_method old_method, method
+      define_method method do |*args|
+        key = "#{method}_#{args.map(&:to_s).join("_")}"
+        stored_value = memo_store[key]
+        unless stored_value && stored_value[:expiration] > DateTime.now
+          raw_result = send(old_method, *args)
+          stored_value = {value: raw_result, expiration: DateTime.now + options[:ttl]}
+          memo_store[key] = stored_value
+        end
+        stored_value[:value]
+      end
+    end
+    private :_memoize
   end
 end
--- spec/lib/cache_in_spec.rb
+++ spec/lib/cache_in_spec.rb
@@ -28,5 +28,59 @@ describe CacheIn do
         obj.life_universe_everything
       end.not_to change(obj, :execution_count)
     end
+
+    it 'can be applied to multiple methods' do
+      class MyObject
+        def execution_counts
+          @execution_counts ||= Hash.new{|h, k| h[k] = 0}
+        end
+
+        def method_1
+          execution_counts[:method_1] += 1
+          44
+        end
+
+
+        def method_2
+          execution_counts[:method_2] += 1
+          'XLIV'
+        end
+
+        memoize [:method_1, :method_2]
+      end
+
+      obj = MyObject.new
+      expect(obj.method_1).to eq(44)
+      expect(obj.method_2).to eq('XLIV')
+
+      obj.method_1
+      obj.method_2
+
+      expect(obj.execution_counts[:method_1]).to eq(1)
+      expect(obj.execution_counts[:method_2]).to eq(1)
+    end
+
+    it 'calls the original method again after the ttl has expired' do
+      class MyObject
+        attr_reader :execution_count
+
+        def life_universe_everything
+          @execution_count = (@execution_count || 0) + 1
+          42
+        end
+        memoize :life_universe_everything, ttl: 2.minutes
+      end
+
+      obj = MyObject.new
+      expect(obj.life_universe_everything).to eq(42)
+      expect(obj.execution_count).to eq(1)
+      expect do
+        obj.life_universe_everything
+      end.not_to change(obj, :execution_count)
+      Timecop.travel(DateTime.now + 2.minutes)
+      expect do
+        obj.life_universe_everything
+      end.to change(obj, :execution_count).by(1)
+    end
   end
 end
