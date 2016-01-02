9551da71915488ccef1834a43901907bfb27d579
Doug Knight <dougbknight@gmail.com>
2016-01-02 11:11:44 -0600

    remove unneeded underscore extensions, add specs

--- app/assets/javascripts/underscore_extensions.js
+++ app/assets/javascripts/underscore_extensions.js
@@ -28,13 +28,6 @@ _.mixin({
     }
     return result;
   },
-  padLeft: function(value, totalLength, character) {
-    var currentLength = value == null ? 0 : value.length;
-    var padLength = totalLength - currentLength;
-    if (padLength > 0)
-      return _.newString(character || " ", padLength) + value;
-    return value;
-  },
   parseDate: function(value) {
     var match = (/(\d{4})-(\d{2})-(\d{2})/).exec(value);
     if (match) {
@@ -45,48 +38,10 @@ _.mixin({
     }
     return new Date(value);
   },
-  sortedIndexDesc: function(list, value, iterator, context) {
-    var reversedCopy = _.toArray(list).reverse();
-    var reversedIndex = _.sortedIndex(reversedCopy, value, iterator, context);
-    return list.length - reversedIndex;
-  },
-  toIsoDate: function(date) {
-    return date.getFullYear()
-      + "-" + _.padLeft((date.getMonth() + 1) + "", 2, "0")
-      + "-" + _.padLeft(date.getDate() + "", 2, "0")
-  },
-  findByMethod: function(list, methodName, value) {
-    return _.find(list, function(item) {
-      return item[methodName]() == value;
-    });
-  },
   findById: function(list, id) {
-    return _.findByMethod(list, 'id', id);
+    return _.find(list, function(item) { return item.id == id })
   },
   uniqueInt: function() {
     return parseInt(_.uniqueId())
-  },
-  getFromLazyLoadedArray: function(array, id, callback) {
-    if (array.state() == 'loaded') {
-      var result = _.findById(array(), id);
-      callback(result);
-      return;
-    }
-
-    var subscription = null;
-    var timeoutId = window.setTimeout(function() {
-      if (subscription != null) subscription.dispose();
-      callback(null);
-    }, 2000);
-
-    subscription = array.subscribe(function(values) {
-      var result = _.findById(values, id);
-      if (result != null) {
-        window.clearTimeout(timeoutId);
-        subscription.dispose();
-        callback(result);
-      }
-    });
-    array();
   }
 });
